992e026e7591af017d65b4723c83d3d0bfa1efd5
sagar011101 <sagarverlekar2017@gmail.com>
2024-12-12 21:59:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CsvParserComponent from "./CsvParserComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <CsvParserComponent />
+    </div>
+  );
+}
+
+export default App;
